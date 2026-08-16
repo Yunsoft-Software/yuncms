589bb61459ba619c6340f0ce277b2feb1f08bce6
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-08-16 23:14:25 +0300

    core: export context and service contracts

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -4,3 +4,12 @@ export { withTransaction } from './transaction.js';
 export { assertIdentifier, quoteIdentifier } from './identifier.js';
 export { YunCmsDatabaseError, normalizeDatabaseError, isRetryableDatabaseError } from './errors.js';
 export { withDatabaseRetry } from './retry.js';
+export {
+  createAccountability,
+  createPublicAccountability,
+  createSystemAccountability,
+  requireAccountability,
+} from './accountability.js';
+export { createRequestContext } from './context.js';
+export { BaseService } from './services/base-service.js';
+export { createServiceRegistry } from './services/service-registry.js';
