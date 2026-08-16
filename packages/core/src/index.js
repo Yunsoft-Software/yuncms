2b1a52cb23533f970c5b383c8ef3fb6c72aaef4a
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-08-17 01:57:37 +0300

    feat: export direct relation expansion helpers

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -15,6 +15,12 @@ export { createInitialAdmin, findExistingAdmin } from './setup.js';
 export { HookEmitter } from './hooks.js';
 export { createJsonLogger, LEVELS as LOG_LEVELS } from './logger.js';
 export { deleteM2MJunction } from './m2m-lifecycle.js';
+export {
+  MAX_EXPAND_FIELDS,
+  parseExpandInput,
+  readManyWithRelations,
+  readOneWithRelations,
+} from './relation-expansion.js';
 export { SmtpMailer } from './mail/smtp-mailer.js';
 export { LocalStorageDriver, assertStorageKey } from './storage/local-storage-driver.js';
 export { S3StorageDriver } from './storage/s3-storage-driver.js';
