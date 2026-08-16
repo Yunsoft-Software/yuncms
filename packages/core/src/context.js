ea2bb5315b8f5fc07979f7ab11449932ff7851c8
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-08-16 23:14:07 +0300

    core: add request context factory

---
+++ packages/core/src/context.js
@@ -0,0 +1,28 @@
+import { requireAccountability } from './accountability.js';
+
+export function createRequestContext({
+  accountability,
+  services,
+  database,
+  schema = null,
+  logger = console,
+  env = {},
+  emitter = null,
+  requestId = null,
+} = {}) {
+  requireAccountability(accountability);
+
+  if (!services) throw new Error('Service registry is required');
+  if (!database) throw new Error('Database handle is required');
+
+  return Object.freeze({
+    accountability,
+    services,
+    database,
+    schema,
+    logger,
+    env,
+    emitter,
+    requestId,
+  });
+}
