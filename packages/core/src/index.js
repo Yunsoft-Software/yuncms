581eba65545f58a1d087d93570f290d87eca478f
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-08-16 23:25:19 +0300

    core: export pinned transaction helper

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,6 +1,6 @@
 export { loadConfig } from './config.js';
 export { createDatabasePool, pingDatabase, closeDatabasePool } from './database.js';
-export { withTransaction } from './transaction.js';
+export { withTransaction, withConnectionTransaction } from './transaction.js';
 export { assertIdentifier, quoteIdentifier } from './identifier.js';
 export { YunCmsDatabaseError, normalizeDatabaseError, isRetryableDatabaseError } from './errors.js';
 export { withDatabaseRetry } from './retry.js';
