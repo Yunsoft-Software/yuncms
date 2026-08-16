c9cc9d5cb667d2472401b537af2ac3817b3f1ee4
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-08-16 23:22:56 +0300

    core: register items service

--- packages/core/src/services/core-services.js
+++ packages/core/src/services/core-services.js
@@ -1,10 +1,12 @@
 import { CollectionsService } from './collections-service.js';
 import { FieldsService } from './fields-service.js';
+import { ItemsService } from './items-service.js';
 import { RelationsService } from './relations-service.js';
 import { createServiceRegistry } from './service-registry.js';
 
 export function createCoreServiceRegistry() {
   return createServiceRegistry({
+    ItemsService,
     CollectionsService,
     FieldsService,
     RelationsService,
