c7d9fcf0f72d8267102843560325f6dcb75cd64c
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-08-17 04:05:58 +0300

    studio: build into API package

--- apps/studio/vite.config.js
+++ apps/studio/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../../packages/api/studio-dist',
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
   },
