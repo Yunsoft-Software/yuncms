aedaf8c7421dc5f1b37f6ecd3c5ab5593938f2aa
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-08-24 03:28:08 +0300

    feat: register navigation group service

--- packages/core/src/services/core-services.js
+++ packages/core/src/services/core-services.js
@@ -6,6 +6,7 @@ import { ExternalAuthService } from './external-auth-service.js';
 import { FieldsService } from './fields-service.js';
 import { FileReconciliationService } from './file-reconciliation-service.js';
 import { FilesService } from './files-service.js';
+import { NavigationGroupsService } from './navigation-groups-service.js';
 import { PermissionsService } from './permissions-service.js';
 import { RelationsService } from './relations-service.js';
 import { RolesService } from './roles-service.js';
@@ -33,5 +34,6 @@ export function createCoreServiceRegistry() {
     FilesService,
     FileReconciliationService,
     StudioSettingsService,
+    NavigationGroupsService,
   });
 }
