155e4d136490e377ef8220b54e7e1774ff8689dc
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-08-24 03:33:16 +0300

    feat: register navigation translations

--- apps/studio/src/localization.js
+++ apps/studio/src/localization.js
@@ -2,13 +2,15 @@ import { AI_EN } from './locales/ai-en.js';
 import { AI_TR } from './locales/ai-tr.js';
 import { EN as BASE_EN } from './locales/en.js';
 import { TR as BASE_TR } from './locales/tr.js';
+import { NAVIGATION_EN } from './locales/navigation-en.js';
+import { NAVIGATION_TR } from './locales/navigation-tr.js';
 import { UI_EN } from './locales/ui-en.js';
 import { UI_TR } from './locales/ui-tr.js';
 import { UX_EN } from './locales/ux-en.js';
 import { UX_TR } from './locales/ux-tr.js';
 
-export const EN = Object.freeze({ ...BASE_EN, ...UI_EN, ...UX_EN, ...AI_EN });
-export const TR = Object.freeze({ ...BASE_TR, ...UI_TR, ...UX_TR, ...AI_TR });
+export const EN = Object.freeze({ ...BASE_EN, ...UI_EN, ...UX_EN, ...AI_EN, ...NAVIGATION_EN });
+export const TR = Object.freeze({ ...BASE_TR, ...UI_TR, ...UX_TR, ...AI_TR, ...NAVIGATION_TR });
 
 const DICTIONARIES = Object.freeze({ en: EN, tr: TR });
 
