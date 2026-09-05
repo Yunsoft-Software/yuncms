282c1bc431b4196ce4fb559652a5c2fa282fed24
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-09-05 18:52:14 +0300

    feat(i18n): add German UX translations

---
+++ apps/studio/src/locales/ux-de.js
@@ -0,0 +1,34 @@
+export const UX_DE = Object.freeze({
+  'fieldBuilder.displayName': 'Anzeigename',
+  'fieldBuilder.displayNameHint': 'Schreibe ihn natürlich. Leerzeichen, türkische Zeichen und anderer Unicode-Text sind möglich.',
+  'fieldBuilder.apiKey': 'API-Schlüssel',
+  'fieldBuilder.apiKeyHint': 'Wird automatisch für MySQL und die REST-API erzeugt. Du kannst ihn vor dem Erstellen des Feldes bearbeiten.',
+  'fieldBuilder.titleExample': 'Produkttitel',
+  'fieldBuilder.coverImageExample': 'Titelbild',
+  'fieldBuilder.attachmentExample': 'Anhang',
+
+  'filePicker.title': 'Aus Dateien auswählen',
+  'filePicker.description': 'Durchsuche die Dateibibliothek und wähle eine Ressource aus, ohne diesen Bildschirm zu verlassen.',
+  'filePicker.searchPlaceholder': 'Nach Titel, Dateiname oder MIME-Typ suchen…',
+  'filePicker.loading': 'Dateien werden geladen…',
+  'filePicker.noFiles': 'Keine Dateien entsprechen dieser Suche.',
+  'filePicker.noImages': 'Keine Bilder entsprechen dieser Suche.',
+  'filePicker.loadError': 'Dateien konnten nicht geladen werden',
+  'filePicker.useSelected': 'Auswahl verwenden',
+
+  'appearance.faviconFromFiles': 'Favicon',
+  'appearance.faviconFromFilesHint': 'Wähle das Browser-/Tab-Symbol aus Dateien. Wenn nichts festgelegt ist, verwendet YunCMS das offizielle Yunsoft-Symbol.',
+  'appearance.defaultAsset': 'Standardressource wird verwendet',
+  'appearance.defaultLogoHint': 'Designabhängiges Yunsoft-Logo',
+  'appearance.defaultFaviconHint': 'Offizielles Yunsoft-Symbol',
+  'appearance.selectFromFiles': 'Aus Dateien auswählen',
+  'appearance.useDefaultAsset': 'Standard verwenden',
+
+  'files.preview': 'Vorschau',
+  'files.openPreview': 'Vorschau für {file}',
+
+  'dataModel.displayName': 'Anzeigename',
+  'dataModel.displayNameHint': 'Menschenlesbarer Name in Studio. Leerzeichen und Unicode-Zeichen werden unterstützt.',
+  'dataModel.apiKey': 'API-/Datenbankschlüssel',
+  'dataModel.apiKeyHint': 'Stabiler normalisierter Schlüssel für REST-URLs, Payloads und MySQL. Eine spätere Änderung des Anzeigenamens benennt diesen Schlüssel nicht um.',
+});
