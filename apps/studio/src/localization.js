2593495c89f97083d3724b614bd56573df28d80a
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-09-05 19:25:19 +0300

    i18n: register localized collection icon labels

--- apps/studio/src/localization.js
+++ apps/studio/src/localization.js
@@ -15,6 +15,14 @@ import { JA as BASE_JA } from './locales/ja.js';
 import { PT_BR as BASE_PT_BR } from './locales/pt-br.js';
 import { TR as BASE_TR } from './locales/tr.js';
 import { ZH_CN as BASE_ZH_CN } from './locales/zh-cn.js';
+import { ICONS_DE } from './locales/icons-de.js';
+import { ICONS_EN } from './locales/icons-en.js';
+import { ICONS_ES } from './locales/icons-es.js';
+import { ICONS_FR } from './locales/icons-fr.js';
+import { ICONS_JA } from './locales/icons-ja.js';
+import { ICONS_PT_BR } from './locales/icons-pt-br.js';
+import { ICONS_TR } from './locales/icons-tr.js';
+import { ICONS_ZH_CN } from './locales/icons-zh-cn.js';
 import { NAVIGATION_DE } from './locales/navigation-de.js';
 import { NAVIGATION_EN } from './locales/navigation-en.js';
 import { NAVIGATION_ES } from './locales/navigation-es.js';
@@ -64,14 +72,14 @@ import { UX_PT_BR } from './locales/ux-pt-br.js';
 import { UX_TR } from './locales/ux-tr.js';
 import { UX_ZH_CN } from './locales/ux-zh-cn.js';
 
-export const EN = Object.freeze({ ...BASE_EN, ...UI_EN, ...UX_EN, ...AI_EN, ...MCP_EN, ...NAVIGATION_EN, ...REGISTRATION_EN, ...STUDIO_NEXT_EN });
-export const TR = Object.freeze({ ...BASE_TR, ...UI_TR, ...UX_TR, ...AI_TR, ...MCP_TR, ...NAVIGATION_TR, ...REGISTRATION_TR, ...STUDIO_NEXT_TR });
-export const ES = Object.freeze({ ...BASE_ES, ...UI_ES, ...UX_ES, ...AI_ES, ...MCP_ES, ...NAVIGATION_ES, ...REGISTRATION_ES, ...STUDIO_NEXT_ES });
-export const DE = Object.freeze({ ...BASE_DE, ...UI_DE, ...UX_DE, ...AI_DE, ...MCP_DE, ...NAVIGATION_DE, ...REGISTRATION_DE, ...STUDIO_NEXT_DE });
-export const FR = Object.freeze({ ...BASE_FR, ...UI_FR, ...UX_FR, ...AI_FR, ...MCP_FR, ...NAVIGATION_FR, ...REGISTRATION_FR, ...STUDIO_NEXT_FR });
-export const PT_BR = Object.freeze({ ...BASE_PT_BR, ...UI_PT_BR, ...UX_PT_BR, ...AI_PT_BR, ...MCP_PT_BR, ...NAVIGATION_PT_BR, ...REGISTRATION_PT_BR, ...STUDIO_NEXT_PT_BR });
-export const JA = Object.freeze({ ...BASE_JA, ...UI_JA, ...UX_JA, ...AI_JA, ...MCP_JA, ...NAVIGATION_JA, ...REGISTRATION_JA, ...STUDIO_NEXT_JA });
-export const ZH_CN = Object.freeze({ ...BASE_ZH_CN, ...UI_ZH_CN, ...UX_ZH_CN, ...AI_ZH_CN, ...MCP_ZH_CN, ...NAVIGATION_ZH_CN, ...REGISTRATION_ZH_CN, ...STUDIO_NEXT_ZH_CN });
+export const EN = Object.freeze({ ...BASE_EN, ...UI_EN, ...UX_EN, ...ICONS_EN, ...AI_EN, ...MCP_EN, ...NAVIGATION_EN, ...REGISTRATION_EN, ...STUDIO_NEXT_EN });
+export const TR = Object.freeze({ ...BASE_TR, ...UI_TR, ...UX_TR, ...ICONS_TR, ...AI_TR, ...MCP_TR, ...NAVIGATION_TR, ...REGISTRATION_TR, ...STUDIO_NEXT_TR });
+export const ES = Object.freeze({ ...BASE_ES, ...UI_ES, ...UX_ES, ...ICONS_ES, ...AI_ES, ...MCP_ES, ...NAVIGATION_ES, ...REGISTRATION_ES, ...STUDIO_NEXT_ES });
+export const DE = Object.freeze({ ...BASE_DE, ...UI_DE, ...UX_DE, ...ICONS_DE, ...AI_DE, ...MCP_DE, ...NAVIGATION_DE, ...REGISTRATION_DE, ...STUDIO_NEXT_DE });
+export const FR = Object.freeze({ ...BASE_FR, ...UI_FR, ...UX_FR, ...ICONS_FR, ...AI_FR, ...MCP_FR, ...NAVIGATION_FR, ...REGISTRATION_FR, ...STUDIO_NEXT_FR });
+export const PT_BR = Object.freeze({ ...BASE_PT_BR, ...UI_PT_BR, ...UX_PT_BR, ...ICONS_PT_BR, ...AI_PT_BR, ...MCP_PT_BR, ...NAVIGATION_PT_BR, ...REGISTRATION_PT_BR, ...STUDIO_NEXT_PT_BR });
+export const JA = Object.freeze({ ...BASE_JA, ...UI_JA, ...UX_JA, ...ICONS_JA, ...AI_JA, ...MCP_JA, ...NAVIGATION_JA, ...REGISTRATION_JA, ...STUDIO_NEXT_JA });
+export const ZH_CN = Object.freeze({ ...BASE_ZH_CN, ...UI_ZH_CN, ...UX_ZH_CN, ...ICONS_ZH_CN, ...AI_ZH_CN, ...MCP_ZH_CN, ...NAVIGATION_ZH_CN, ...REGISTRATION_ZH_CN, ...STUDIO_NEXT_ZH_CN });
 
 export const DICTIONARIES = Object.freeze({
   en: EN,
